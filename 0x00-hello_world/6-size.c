c51512a755e17a57eb3c90cbbac40ba7505afa25
Maritimsephania <kipkoechsephania@gmail.com>
2022-12-09 06:30:27 -0800

    size of code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+main(void)
+{
+	printf("Size of a char: %i byte(s)\n" size of(char));
+	printf("Size of an int: %i byte(s)\n" size of(int));
+	printf("Size of a long int: %i byte(s)\n" size of(long int));
+		printf("size of long long int: %i bytes(s)\n" size of(long long int));
+		printf("size if a float: %i bytes(s)\n" size of(float));
+		return (0);
+}
