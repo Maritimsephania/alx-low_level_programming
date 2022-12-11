b2ce1ff39daecf323d64a47d08b47e66d978dbb9
Maritimsephania <kipkoechsephania@gmail.com>
2022-12-11 04:27:01 -0800

    conditional

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n" size of(char));
-	printf("Size of an int: %i byte(s)\n" size of(int));
-	printf("Size of a long int: %i byte(s)\n" size of(long int));
-		printf("size of long long int: %i bytes(s)\n" size of(long long int));
-		printf("size if a float: %i bytes(s)\n" size of(float));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+		printf("size of long long int: %i bytes(s)\n", sizeof(long long int));
+		printf("size if a float: %i bytes(s)\n", sizeof(float));
 		return (0);
 }
